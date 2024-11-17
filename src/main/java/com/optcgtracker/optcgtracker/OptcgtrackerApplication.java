3ececd7f76871fe6c4e086908146d4debf6a954a
Afton Slone Jr <267187@NTTDATA.COM>
2024-11-17 14:52:34 -0500

    First Commit

---
+++ src/main/java/com/optcgtracker/optcgtracker/OptcgtrackerApplication.java
@@ -0,0 +1,13 @@
+package com.optcgtracker.optcgtracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OptcgtrackerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OptcgtrackerApplication.class, args);
+	}
+
+}
